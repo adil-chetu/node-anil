4b57259824d76f05b32d5287d04ccc75f96fad9d
Adil Choudhary <adil.chetu@yahoo.com>
2022-12-26 00:28:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const colors = require('colors');
+
+// http.createServer((req, resp)=>{
+//     resp.writeHead(201, 'Created the file')
+//     resp.write('This is the custom server')
+//     resp.end()
+// }).listen(4500);
+
+console.log('package'.red)
